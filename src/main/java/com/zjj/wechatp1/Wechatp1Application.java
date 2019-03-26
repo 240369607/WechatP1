4a88a664a8105ddc67be7aa2c6121789489a462c
DELL <DELL@hello>
2019-03-26 12:22:37 +0800

    初次上传

---
+++ src/main/java/com/zjj/wechatp1/Wechatp1Application.java
@@ -0,0 +1,13 @@
+package com.zjj.wechatp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wechatp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wechatp1Application.class, args);
+	}
+
+}
